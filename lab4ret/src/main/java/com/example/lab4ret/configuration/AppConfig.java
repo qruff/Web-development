42aa7a4625993a3c0a529d8fb6bd4541f731be35
qruff <99872054+qruff@users.noreply.github.com>
2023-05-30 00:01:13 +0300

    Delete lab4ret directory

--- lab4ret/src/main/java/com/example/lab4ret/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab4ret.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.lab4ret")
-public class AppConfig {
-}
